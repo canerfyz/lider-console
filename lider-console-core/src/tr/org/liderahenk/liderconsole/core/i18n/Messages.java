b8968afb05b893abb9b45171ea4b1de7177895b4
emrekgn <emrekaganakkaya@gmail.com>
2016-03-21 19:28:33 +0200

    updated messages & icons

---
+++ lider-console-core/src/tr/org/liderahenk/liderconsole/core/i18n/Messages.java
@@ -0,0 +1,30 @@
+package tr.org.liderahenk.liderconsole.core.i18n;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Provides i18n messages for Lider Console Core strings.
+ * 
+ * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
+ *
+ */
+public class Messages extends NLS {
+
+	private static final String BUNDLE_NAME = "tr.org.liderahenk.liderconsole.core.i18n.messages";
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+	private Messages() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
