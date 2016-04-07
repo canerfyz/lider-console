f36f84800f2417b3ebccbaeeea07071daaa326a2
emrekgn <emrekaganakkaya@gmail.com>
2016-04-07 19:23:23 +0300

    added new messages & impl default title area dialog

---
+++ lider-console-core/src/tr/org/liderahenk/liderconsole/core/dialogs/DefaultLiderTitleAreaDialog.java
@@ -0,0 +1,33 @@
+package tr.org.liderahenk.liderconsole.core.dialogs;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+
+import tr.org.liderahenk.liderconsole.core.i18n.Messages;
+
+/**
+ * Provides i18n button names for extending dialogs. Other dialog
+ * implementations may extend this class.
+ * 
+ * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
+ *
+ */
+public abstract class DefaultLiderTitleAreaDialog extends TitleAreaDialog {
+
+	public DefaultLiderTitleAreaDialog(Shell parentShell) {
+		super(parentShell);
+	}
+
+	/**
+	 * Override parent method in order to use i18n button names.
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		// create OK and Cancel buttons by default
+		createButton(parent, IDialogConstants.OK_ID, Messages.getString("OK"), true);
+		createButton(parent, IDialogConstants.CANCEL_ID, Messages.getString("CANCEL"), false);
+	}
+
+}
