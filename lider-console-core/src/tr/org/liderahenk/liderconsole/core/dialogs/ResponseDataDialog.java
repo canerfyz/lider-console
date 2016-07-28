4973932891b06957d2616ae409aef6502d8819e3
emrekgn <emre.akkaya@agem.com.tr>
2016-07-28 18:34:13 +0300

    display task response data in detailed dialog

--- lider-console-core/src/tr/org/liderahenk/liderconsole/core/dialogs/ResponseDataDialog.java
+++ lider-console-core/src/tr/org/liderahenk/liderconsole/core/dialogs/ResponseDataDialog.java
@@ -3,6 +3,7 @@ package tr.org.liderahenk.liderconsole.core.dialogs;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonParseException;
@@ -24,6 +25,7 @@ import org.eclipse.swt.widgets.Text;
 
 import tr.org.liderahenk.liderconsole.core.i18n.Messages;
 import tr.org.liderahenk.liderconsole.core.model.CommandExecutionResult;
+import tr.org.liderahenk.liderconsole.core.utils.SWTResourceManager;
 import tr.org.liderahenk.liderconsole.core.xmpp.enums.ContentType;
 
 /**
@@ -45,14 +47,9 @@ public class ResponseDataDialog extends DefaultLiderTitleAreaDialog {
 	public void create() {
 		super.create();
 		setTitle(Messages.getString("COMMAND_EXECUTION_RESULT"));
-		setMessage(
-				(result.getResponseMessage() != null ? result.getResponseMessage()
-						+ " "
-						: "")
-						+ (result.getCreateDate() != null ? result.getCreateDate()
-								+ " "
-								: "") + result.getResponseCode().getMessage(),
-				IMessageProvider.INFORMATION);
+		setMessage((result.getResponseMessage() != null ? result.getResponseMessage() + " " : "")
+				+ (result.getCreateDate() != null ? result.getCreateDate() + " " : "")
+				+ result.getResponseCode().getMessage(), IMessageProvider.INFORMATION);
 	}
 
 	@Override
@@ -60,35 +57,44 @@ public class ResponseDataDialog extends DefaultLiderTitleAreaDialog {
 
 		Composite composite = new Composite(parent, SWT.NONE);
 		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		composite.setLayout(new GridLayout(1, false));
+		composite.setLayout(new GridLayout(2, false));
 
 		byte[] responseData = result.getResponseData();
 		ContentType contentType = result.getContentType();
 
 		if (responseData == null || responseData.length == 0) {
-			Text txtParams = new Text(composite, SWT.BORDER | SWT.READ_ONLY
-					| SWT.MULTI | SWT.V_SCROLL);
-			txtParams
-					.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+			Label lblResult = new Label(composite, SWT.NONE);
+			lblResult.setFont(SWTResourceManager.getFont("Sans", 9, SWT.BOLD));
+			lblResult.setText(Messages.getString("RESPONSE_DATA"));
+			Text txtParams = new Text(composite, SWT.BORDER | SWT.READ_ONLY | SWT.MULTI | SWT.V_SCROLL);
+			txtParams.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 			txtParams.setText(Messages.getString("RESPONSE_DATA_EMPTY"));
 		}
 		if (ContentType.APPLICATION_JSON == contentType) {
 			ObjectMapper mapper = new ObjectMapper();
 			try {
-				String msg = mapper
-						.defaultPrettyPrintingWriter()
-						.writeValueAsString(
-								mapper.readValue(
-										responseData,
-										0,
-										responseData.length,
-										new TypeReference<HashMap<String, Object>>() {
-										}));
-				Text txtParams = new Text(composite, SWT.BORDER | SWT.READ_ONLY
-						| SWT.MULTI | SWT.V_SCROLL);
-				txtParams.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
-						true));
-				txtParams.setText(msg);
+				HashMap<String, Object> resultMap = mapper.readValue(responseData, 0, responseData.length,
+						new TypeReference<HashMap<String, Object>>() {
+						});
+				for (Entry<String, Object> entry : resultMap.entrySet()) {
+					// Property name
+					Label lblPropName = new Label(composite, SWT.NONE);
+					lblPropName.setFont(SWTResourceManager.getFont("Sans", 9, SWT.BOLD));
+					lblPropName.setText(entry.getKey());
+					// Property value
+					String value = entry.getValue() != null ? entry.getValue().toString() : "";
+					int style = SWT.BORDER | SWT.READ_ONLY;
+					if (value.length() > 100) {
+						style |= SWT.MULTI | SWT.V_SCROLL;
+					}
+					Text txtPropValue = new Text(composite, style);
+					GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
+					if (value.length() > 100) {
+						gridData.heightHint = 200;
+					}
+					txtPropValue.setLayoutData(gridData);
+					txtPropValue.setText(value);
+				}
 			} catch (JsonGenerationException e) {
 				e.printStackTrace();
 			} catch (JsonMappingException e) {
@@ -98,20 +104,21 @@ public class ResponseDataDialog extends DefaultLiderTitleAreaDialog {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		} else if (ContentType.TEXT_PLAIN == contentType
-				|| ContentType.TEXT_HTML == contentType) {
-			Text txtParams = new Text(composite, SWT.BORDER | SWT.READ_ONLY
-					| SWT.MULTI | SWT.V_SCROLL);
-			txtParams
-					.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		} else if (ContentType.TEXT_PLAIN == contentType || ContentType.TEXT_HTML == contentType) {
+			Label lblResult = new Label(composite, SWT.NONE);
+			lblResult.setFont(SWTResourceManager.getFont("Sans", 9, SWT.BOLD));
+			lblResult.setText(Messages.getString("RESPONSE_DATA"));
+			Text txtParams = new Text(composite, SWT.BORDER | SWT.READ_ONLY | SWT.MULTI | SWT.V_SCROLL);
+			txtParams.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 			txtParams.setText(new String(responseData));
-		} else if (ContentType.IMAGE_JPEG == contentType
-				|| ContentType.IMAGE_PNG == contentType) {
+		} else if (ContentType.IMAGE_JPEG == contentType || ContentType.IMAGE_PNG == contentType) {
+			Label lblResult = new Label(composite, SWT.NONE);
+			lblResult.setFont(SWTResourceManager.getFont("Sans", 9, SWT.BOLD));
+			lblResult.setText(Messages.getString("RESPONSE_DATA"));
 			Label lblImage = new Label(composite, SWT.BORDER);
 			lblImage.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 			lblImage.setImage(createImage(responseData));
 		}
-		// TODO display file contents!
 		return composite;
 	}
 
@@ -124,14 +131,12 @@ public class ResponseDataDialog extends DefaultLiderTitleAreaDialog {
 	private Image createImage(byte[] responseData) {
 		int width = 300;
 		int height = 200;
-		Image image = new Image(Display.getDefault(), new ByteArrayInputStream(
-				responseData));
+		Image image = new Image(Display.getDefault(), new ByteArrayInputStream(responseData));
 		Image scaled = new Image(Display.getDefault(), width, height);
 		GC gc = new GC(scaled);
 		gc.setAntialias(SWT.ON);
 		gc.setInterpolation(SWT.HIGH);
-		gc.drawImage(image, 0, 0, image.getBounds().width,
-				image.getBounds().height, 0, 0, width, height);
+		gc.drawImage(image, 0, 0, image.getBounds().width, image.getBounds().height, 0, 0, width, height);
 		gc.dispose();
 		image.dispose();
 		return scaled;
