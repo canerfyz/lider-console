543c750cfef46b084f1354e6d9db452d1869d3f7
emrekgn <emre.akkaya@agem.com.tr>
2016-09-09 17:35:30 +0300

    findbugs fix

--- lider-console-rcp/src/tr/org/liderahenk/liderconsole/rcp/Activator.java
+++ lider-console-rcp/src/tr/org/liderahenk/liderconsole/rcp/Activator.java
@@ -12,9 +12,6 @@ public class Activator extends AbstractUIPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "tr.org.liderahenk.liderconsole.rcp"; //$NON-NLS-1$
 
-	// The shared instance
-	private static Activator plugin;
-	
 	/**
 	 * The constructor
 	 */
@@ -23,36 +20,30 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
+	 * BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		plugin = this;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.
+	 * BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
 		super.stop(context);
 	}
 
 	/**
-	 * Returns the shared instance
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
 	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
